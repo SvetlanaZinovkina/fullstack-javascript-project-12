0a9861cc37aedf2d6b072e782af4703531cdf277
Svetlana <pidgin66@gmail.com>
2024-02-20 16:36:43 +0300

    step 4

--- frontend/src/components/channels.jsx
+++ frontend/src/components/channels.jsx
@@ -31,7 +31,7 @@ const Channels = () => {
   const handleChannelClick = (id) => dispatch(setActiveChannel(id));
 
   return (
-    <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
+    <div className="col-4 col-md-2 border-end px-0 bg-light flex-column d-flex">
       <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
         <b>Каналы</b>
         <button type="button" className="p-0 text-primary btn btn-group-vertical">
