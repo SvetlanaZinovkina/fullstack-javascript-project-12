cd80fefe41d108287ee76e10f3f5cb5edd226d65
Svetlana <pidgin66@gmail.com>
2024-03-05 20:49:46 +0300

    add exit btn

--- frontend/src/components/navigation.jsx
+++ frontend/src/components/navigation.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router';
 
 const Navigation = () => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+  const token = localStorage.getItem('token');
+  const resetUser = () => {
+    localStorage.clear();
+    navigate('/login');
+  };
   return (
     <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-warning bg-opacity-25">
       <div className="container">
         <a className="navbar-brand" href="/">{t('navBar.text')}</a>
-        <button className="btn btn-danger" type="submit">{t('navBar.exitBtn')}</button>
+        {token && <button className="btn btn-danger" type="submit" onClick={resetUser}>{t('navBar.exitBtn')}</button>}
       </div>
 
     </nav>
