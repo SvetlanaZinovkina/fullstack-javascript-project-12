4ec416dccd28171b33e762fca4d0d71be96a041f
Svetlana <pidgin66@gmail.com>
2024-03-30 16:44:42 +0300

    refactoring i18n init

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App.jsx';
-import './i18n.js';
 import store from './services/store.js';
 
-const appContainer = document.createElement('div');
-appContainer.className = 'h-100';
-
-document.body.appendChild(appContainer);
-
-const root = ReactDOM.createRoot(appContainer);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <App />
